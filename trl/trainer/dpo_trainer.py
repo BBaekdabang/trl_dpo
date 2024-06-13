745b14483a4305a6abcee5c22f5028c81a1af7df
BBaekdabang <113493692+BBaekdabang@users.noreply.github.com>
2024-06-13 17:29:36 +0900

    Update dpo_trainer.py

--- trl/trainer/dpo_trainer.py
+++ trl/trainer/dpo_trainer.py
@@ -631,20 +631,19 @@ class DPOTrainer(Trainer):
                 elif k.endswith("_attention_mask"):
                     pad_value = 0
                 concatenated_key = k.replace("chosen", "concatenated")
-                print(concatenated_key)
                 concatenated_batch[concatenated_key] = pad_to_length(batch[k], max_length, pad_value=pad_value)
-        print('********')
-        print(batch)  
-        print('************')
+
         for k in batch:
             print(k)
             if k.startswith("rejected") and isinstance(batch[k], torch.Tensor):
+                print(k)
                 if "labels" in k or is_encoder_decoder:
                     pad_value = label_pad_token_id
                 elif k.endswith("_input_ids"):
                     pad_value = padding_value
                 elif k.endswith("_attention_mask"):
                     pad_value = 0
+                
                 concatenated_key = k.replace("rejected", "concatenated")
                 concatenated_batch[concatenated_key] = torch.cat(
                     (
